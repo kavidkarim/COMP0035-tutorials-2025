944c10870e2b6015ff6fd8ac60ba9221467700bd
kavi <kavidkarim@gmail.com>
2025-10-10 17:12:11 +0100

    Tutorial 2.03 complete

--- src/activities/my_code/tutorial_2_pandas.py
+++ src/activities/my_code/tutorial_2_pandas.py
@@ -1,5 +1,7 @@
 def main():
-    tutorial_202()
+    csv_df, xlsx_df_0, xlsx_df_1 = tutorial_202()
+    tutorial_203(csv_df)
+
 
 def tutorial_201():
     #python file structures
@@ -21,6 +23,7 @@ def tutorial_201():
         print("CSV file not found")
 
 def tutorial_202():
+
     #tutorial 2.02 - Pandas dataframes
     import pandas as pd
     
@@ -36,6 +39,31 @@ def tutorial_202():
     csv_df = pd.read_csv(paralympics_csv)
     xlsx_df_0 = pd.read_excel(paralympics_xlsx)
     xlsx_df_1 = pd.read_excel(paralympics_xlsx, sheet_name=1)
+
+    return(csv_df, xlsx_df_0, xlsx_df_1)
+
+def tutorial_203(dataframe):
+    import pandas as pd
+
+    """This function will return info about a passed in dataframe
+    
+    Parameters:
+    dataframe (dataframe): dataframe
+
+    Returns:
+    str: Description of the dataframe
+
+    """
+
+    print(dataframe.shape)
+    pd.set_option("display.max_columns", None)
+    print(dataframe.head)
+    print(dataframe.tail)
+    print(dataframe.columns)
+    print(dataframe.dtypes)
+    print(dataframe.info)
+    print(dataframe.describe)
+
 
 
 if __name__ == "__main__":
